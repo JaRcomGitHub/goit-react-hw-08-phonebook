cb8a74d30880368c09cf9a69df38082da48280b2
JaRcomGitHub <jarcom@ukr.net>
2022-12-04 10:07:37 +0200

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { phonebookSlice } from "./phonebookSlice";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const phonebookReducer = persistReducer(persistConfig, phonebookSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    phonebook: phonebookSlice.reducer,
+    phonebook: phonebookReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE,
+          PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
+
+export const persistor = persistStore(store);
